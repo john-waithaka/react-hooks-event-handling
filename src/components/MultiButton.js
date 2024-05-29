68eaf599ccdb8e27907a09928d7afa8766d96f74
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-05-29 14:59:03 +0300

    event handlers

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -1,5 +1,39 @@
 import React from "react";
 
+// function MultiButton() {
+//   function handleClick(number) {
+//     console.log(`Button ${number} was clicked`);
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={handleClick}>Button 1</button>
+//       <button onClick={handleClick}>Button 2</button>
+//       <button onClick={handleClick}>Button 3</button>
+//     </div>
+//   );
+// }
+
+// export default MultiButton;
+
+// //function invoked by default so the console message appears everytime when you refresh/you render the component multibutton
+// function MultiButton() {
+//   function handleClick(number) {
+//     console.log(`Button ${number} was clicked`);
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={handleClick(1)}>Button 1</button>
+//       <button onClick={handleClick(2)}>Button 2</button>
+//       <button onClick={handleClick(3)}>Button 3</button>
+//     </div>
+//   );
+// }
+
+// export default MultiButton;
+
+//this handles and logs each button click separately, unless you click it twice
 function MultiButton() {
   function handleClick(number) {
     console.log(`Button ${number} was clicked`);
@@ -7,9 +41,9 @@ function MultiButton() {
 
   return (
     <div>
-      <button onClick={handleClick}>Button 1</button>
-      <button onClick={handleClick}>Button 2</button>
-      <button onClick={handleClick}>Button 3</button>
+      <button onClick={() => handleClick(1)}>Button 1</button>
+      <button onClick={() => handleClick(2)}>Button 2</button>
+      <button onClick={() => handleClick(3)}>Button 3</button>
     </div>
   );
 }
